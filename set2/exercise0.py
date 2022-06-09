16e85dd2f05e47ce7f389a6629eb4ce850d7e35f
Berny Yan <berny.yan@gmail.com>
2022-06-09 17:58:19 +1000

    yeah

--- set2/exercise0.py
+++ set2/exercise0.py
@@ -17,6 +17,11 @@ obvious answer, that's because it is!
 """
 
 
+from curses.ascii import isupper
+from sre_constants import AT_END_STRING
+from string import ascii_uppercase
+
+
 def add_1(a_number):
     """Return a number that is 1 bigger than number given.
 
@@ -42,7 +47,7 @@ def add_5(a_number):
         return a_number plus five
     except expressed in python, not english
     """
-    the_answer = None
+    the_answer = a_number + 5
     return the_answer
 
 
@@ -51,7 +56,7 @@ def adder(a_number, another_number):
 
     Same as above, but with any two numbers.
     """
-    the_answer = None
+    the_answer = a_number + another_number
     return the_answer
 
 
@@ -66,7 +71,7 @@ def shout(a_string):
           they all do different things. You'll need to actually read the
           docs to find out which one you actually need.
     """
-    the_answer = None
+    the_answer = a_string.uppercase()
     return the_answer
 
 
@@ -79,7 +84,7 @@ def really_shout(a_string):
     that you've reused the function you already wrote.
     Look up how to 'concatinate' strings to make this happen.
     """
-    the_answer = None
+    the_answer = a_string.uppercase() 
     return the_answer
 
 
@@ -95,7 +100,7 @@ def shout_with_a_number(a_string, a_number):
           challenge, see if you can make the test pass with at least two ways
           of doing the same job.
     """
-    the_answer = None
+    the_answer = a_string.upper() + "" + str(a_number)
     return the_answer
 
 
